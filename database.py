c13e70caaa504eee88c9d68ec0e468e5709437a8
Chityanj <chityanj@gmail.com>
2023-11-25 14:18:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = 'postgresql://chityanjs:Nfqyolj4Swz1@ep-plain-wildflower-79539467.ap-southeast-1.aws.neon.tech/tess?sslmode=require'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
