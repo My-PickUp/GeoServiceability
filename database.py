beadea30cca798cc390405010fde6218fbe6b361
Chityanj <chityanj@gmail.com>
2023-11-25 14:26:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from .config import settings
+from config import settings
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://chityanjs:Nfqyolj4Swz1@ep-plain-wildflower-79539467.ap-southeast-1.aws.neon.tech/tess?sslmode=require'
 
